c5cb31de608ec18b140a02fe34d80de5d9656d9f
Jitesh Singh <jiteshsingh7983@gmail.com>
2025-08-27 23:46:31 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-
 import "prismjs/themes/prism-tomorrow.css";
 import Editor from "react-simple-code-editor";
 import prism from 'prismjs';
@@ -19,9 +18,7 @@ function App() {
 
   async function reviewCode() {
     try {
-      // use env var in production, fallback to localhost in dev
       const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-
       const response = await axios.post(`${API_URL}/ai/get-review`, { code });
       setReview(response.data.review);
     } catch (error) {
@@ -31,35 +28,33 @@ function App() {
   }
 
   return (
-    <>
-      <main>
-        <div className="left">
-          <div className="code">
-            <Editor
-              value={code}
-              onValueChange={code => setCode(code)}
-              highlight={code => prism.highlight(code, prism.languages.js, 'js')}
-              padding={10}
-              style={{
-                fontFamily: '"Fira code", "Fira Mono", monospace',
-                fontSize: 16,
-                backgroundColor: 'transparent',
-                border: '1px solid #ddd',
-                borderRadius: '5px',
-                height: '100%',
-                width: '100%',
-              }}
-            />
-          </div>
-          <div onClick={reviewCode} className="review">Review</div>
+    <main>
+      <div className="left">
+        <div className="code">
+          <Editor
+            value={code}
+            onValueChange={setCode}
+            highlight={code => prism.highlight(code, prism.languages.js, 'js')}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 16,
+              backgroundColor: 'transparent',
+              border: '1px solid #ddd',
+              borderRadius: '5px',
+              height: '100%',
+              width: '100%',
+            }}
+          />
         </div>
-        <div className="right">
-          <Markdown rehypePlugins={[rehypeHighlight]}>
-            {review}
-          </Markdown>
-        </div>
-      </main>
-    </>
+        <div onClick={reviewCode} className="review">Review</div>
+      </div>
+      <div className="right">
+        <Markdown rehypePlugins={[rehypeHighlight]}>
+          {review}
+        </Markdown>
+      </div>
+    </main>
   );
 }
 
